1652936cee26bb033f1ea2da20cab47df787f325
Aniket Dubey <aniketdubey@Anikets-MacBook-Pro.local>
2026-06-19 00:52:46 +0530

    chore: product review schema update

--- backend/app/schema/product_review.py
+++ backend/app/schema/product_review.py
@@ -1,26 +1,36 @@
 amazon_product_reviews_schema = {
     "name": "Amazon Product Reviews",
-    "baseSelector": "div.a-section.reviews-content",
+    "baseSelector": "ul#localTopReviewsList",
     "fields": [
         {
             "name": "reviews",
             "type": "list",
-            "selector": "div.a-section.celwidget",
+            "selector": "div.a-section.aok-relative",
             "fields": [
                 {
                     "name": "rating",
                     "type": "text",
-                    "selector": "div:nth-of-type(2)>h5>a>i",
+                    "selector": "div:nth-of-type(2)>i>span",
                 },
                 {
                     "name": "title",
                     "type": "text",
-                    "selector": "div:nth-of-type(2)>h5>a>span:nth-child(3)",
+                    "selector": "a>h5",
                 },
                 {
-                    "name": "text",
+                    "name": "review date",
                     "type": "text",
-                    "selector": "div:nth-of-type(4)",
+                    "selector": "div:nth-of-type(3)>span",
+                },
+                {
+                    "name": "purchase verification",
+                    "type": "text",
+                    "selector": "div:nth-of-type(4)>span",
+                },
+                {
+                    "name": "purchase verification",
+                    "type": "text",
+                    "selector": "div:nth-of-type(5)>div>div>div>div>div>div>div>div>p>span",
                 },
             ],
         },
